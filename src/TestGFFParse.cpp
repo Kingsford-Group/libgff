6cdcb7a59eb803bfa280070c44b07ff3706e3629
Rob <robp@cs.cmu.edu>
2014-06-21 23:13:15 -0400

    Changed compile flags; build test program

--- src/TestGFFParse.cpp
+++ src/TestGFFParse.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 #include "gff.h"
 
-#include <cstdio>
-
 int main(int argc, char* argv[]) {
+
+    if (argc == 1 or argc > 2) {
+        std::cerr << "Usage: TestGFFParse input\n";
+        std::exit(1);
+    }
 
     GffReader reader(argv[1]);
     reader.readAll(true);
 
-
     std::cerr << "had count of " << reader.gflst.Count() << "\n";
     size_t nfeat = reader.gflst.Count();
     for (size_t i=0; i < nfeat; ++i) {
-       auto f = reader.gflst[i];
+       GffObj* f = reader.gflst[i];
        if (f->isTranscript()) {
 
            std::cout << f->getID() << '\t' << f->getGeneID() << '\t';
@@ -25,6 +29,6 @@ int main(int argc, char* argv[]) {
            std::cout << "\n";
        }
     }
-
+    std::exit(0);
 }
 
